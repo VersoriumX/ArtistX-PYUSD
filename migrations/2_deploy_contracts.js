fd8467c65810cef88511102390c71dc0cb1900fc
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-01-13 13:44:54 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,7 +6,7 @@ module.exports = async function(deployer) {
 
   await deployer.deploy(PYUSD);
   const proxy = await deployer.deploy(Proxy, PYUSD.address);
-  const proxiedPYUSD = await PYUSD.at(proxy.address);
+  const proxiedPYUSD = await PYUSD.at(proxy.0x608cfC1575b56a82a352f14d61be100FA9709D75);
   await proxy.changeAdmin("0x591C13850C672c5713A8774E34DDe8777BA72Ef4");
   await proxiedPYUSD.initialize();
 };
